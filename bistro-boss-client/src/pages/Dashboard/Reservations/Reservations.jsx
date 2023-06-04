c341aa26bd0c02238d345430becd42839418d834
Iftekher Aziz <azizchy02@gmail.com>
2023-06-04 21:01:57 +0600

    Display Dashboard charts

--- bistro-boss-client/src/pages/Dashboard/Reservations/Reservations.jsx
+++ bistro-boss-client/src/pages/Dashboard/Reservations/Reservations.jsx
@@ -1,10 +1,14 @@
+import { Helmet } from "react-helmet-async";
 
 
 const Reservations = () => {
     return (
-        <div>
-            <h2>This is reservations</h2>
-        </div>
+      <div className="max-h-screen pt-0 p-10 w-full">
+        <Helmet>
+          <title>Bistro Boss | Reservartion</title>
+        </Helmet>
+        <h2 className="text-2xl mt-10 font-medium text-center">Reservations Data</h2>
+      </div>
     );
 };
 
--- bistro-boss-client/src/pages/Dashboard/UserHome/UserHome.jsx
+++ bistro-boss-client/src/pages/Dashboard/UserHome/UserHome.jsx
@@ -2,11 +2,11 @@ import { Helmet } from "react-helmet-async";
 
 const UserHome = () => {
   return (
-    <div>
+    <div className="max-h-screen pt-0 p-10 w-full">
       <Helmet>
         <title>Bistro Boss | User Home</title>
       </Helmet>
-      <h2>This is user homepage</h2>
+      <h2 className="text-2xl mt-10 font-medium text-center">User Dashboard</h2>
     </div>
   );
 };
